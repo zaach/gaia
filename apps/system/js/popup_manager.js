3bc78d2d8df472808821785d69af09548121bbea
Ben Adida <ben@adida.net>
2012-10-03 12:32:48 -0700

    milestone 1

--- apps/system/js/popup_manager.js
+++ apps/system/js/popup_manager.js
@@ -52,7 +52,7 @@ var PopupManager = {
     dataset.frameName = name;
     dataset.frameOrigin = origin;
 
-    // this seems needed, or an override to origin in close()
+    // this seems needed, or an override to origin in close() [Ben Adida]
     this._currentOrigin = origin;
 
     this.container.appendChild(popup);
