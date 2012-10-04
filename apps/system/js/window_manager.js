541dc17d6dc2370a8553cf169f83fab7d0c454af
Alberto Pastor <albert.pastor@gmail.com>
2012-10-03 17:43:21 -0700

    Bug 801561 Follow up Gaia TrustedUIManager r=ferj
    Nit fixing

--- apps/system/js/window_manager.js
+++ apps/system/js/window_manager.js
@@ -657,6 +657,27 @@ var WindowManager = (function() {
     return runningApps[homescreen].frame;
   }
 
+  // Hide current app
+  function hideCurrentApp(callback) {
+    if (displayedApp == null || displayedApp == homescreen)
+      return;
+    var frame = getAppFrame(displayedApp);
+    frame.classList.add('hideBottom');
+    frame.classList.remove('restored');
+    if (callback) {
+      frame.addEventListener('transitionend', function execCallback() {
+        frame.removeEventListener('transitionend', execCallback);
+        callback();
+      });
+    }
+  }
+
+  function restoreCurrentApp() {
+    var frame = getAppFrame(displayedApp);
+    frame.classList.add('restored');
+    frame.classList.remove('hideBottom');
+  }
+
   // Switch to a different app
   function setDisplayedApp(origin, callback) {
     var currentApp = displayedApp, newApp = origin || homescreen;
@@ -1007,7 +1028,6 @@ var WindowManager = (function() {
         }
       }
     }
-
     switch (e.detail.type) {
       // mozApps API is asking us to launch the app
       // We will launch it in foreground
@@ -1343,6 +1363,12 @@ var WindowManager = (function() {
     getAppFrame: getAppFrame,
     getRunningApps: function() {
       return runningApps;
-    }
+    },
+    setDisplayedApp: setDisplayedApp,
+    getCurrentDisplayedApp: function() {
+      return runningApps[displayedApp];
+    },
+    hideCurrentApp: hideCurrentApp,
+    restoreCurrentApp: restoreCurrentApp
   };
 }());
